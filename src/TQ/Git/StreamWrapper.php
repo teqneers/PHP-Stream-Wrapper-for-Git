6fdf197c19fcf8e19c6f87e845bc0bd77401b3d6
Stefan Gehrig <stefan.gehrig.hn@googlemail.com>
2011-07-26 21:11:42 +0200

    added methods stubs for stream wrapper according to http://www.php.net/manual/en/class.streamwrapper.php
    Signed-off-by: Stefan Gehrig <stefan.gehrig.hn@googlemail.com>

--- src/TQ/Git/StreamWrapper.php
+++ src/TQ/Git/StreamWrapper.php
@@ -11,6 +11,12 @@ class StreamWrapper
 
     /**
      *
+     * @var resource
+     */
+    public $context;
+
+    /**
+     *
      * @param   string              $protocol
      * @param   Binary|string    $binary
      */
@@ -36,10 +42,8 @@ class StreamWrapper
     /**
      *
      */
-    protected function __construct()
+    public function __construct()
     {
-        // StreamWrapper should be instantiatable from userland code
-        // use StreamWrapper::register instead
     }
 
     /**
@@ -49,6 +53,209 @@ class StreamWrapper
     protected function getBinary()
     {
         return self::$binary;
+    }
+
+    /**
+     *
+     * @return  boolean
+     */
+    public function dir_closedir()
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @param   integer  $options
+     * @return  boolean
+     */
+    public function dir_opendir($path, $options)
+    {
+    }
+
+    /**
+     *
+     * @return  string
+     */
+    public function dir_readdir()
+    {
+    }
+
+    /**
+     *
+     * @return  boolean
+     */
+    public function dir_rewinddir()
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @param   integer  $mode
+     * @param   integer  $options
+     * @return  boolean
+     */
+    public function mkdir($path, $mode, $options)
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path_from
+     * @param   string   $path_to
+     * @return  boolean
+     */
+    public function rename($path_from, $path_to)
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @param   integer  $options
+     * @return  boolean
+     */
+    public function rmdir($path, $options)
+    {
+    }
+
+    /**
+     *
+     * @param   integer  $cast_as
+     * @return  resource
+     */
+    public function stream_cast($cast_as)
+    {
+    }
+
+    /**
+     *
+     */
+    public function stream_close()
+    {
+    }
+
+    /**
+     *
+     * @return  boolean
+     */
+    public function stream_eof()
+    {
+    }
+
+    /**
+     *
+     * @return  boolean
+     */
+    public function stream_flush()
+    {
+    }
+
+    /**
+     *
+     * @param   integer  $operation
+     * @return  boolean
+     */
+    public function stream_lock($operation)
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @param   integer  $option
+     * @param   integer  $var
+     * @return  boolean
+     */
+    public function stream_metadata($path, $option, $var)
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @param   string   $mode
+     * @param   integer  $options
+     * @param   string   $opened_path
+     * @return  boolean
+     */
+    public function stream_open($path, $mode, $options, &$opened_path)
+    {
+    }
+
+    /**
+     *
+     * @param   integer  $count
+     * @return  string
+     */
+    public function stream_read($count)
+    {
+    }
+
+    /**
+     *
+     * @param   integer  $offset
+     * @param   integer  $whence
+     * @return  boolean
+     */
+    public function stream_seek($offset, $whence = SEEK_SET)
+    {
+    }
+
+    /**
+     *
+     * @param   integer  $option
+     * @param   integer  $arg1
+     * @param   integer  $arg2
+     * @return  boolean
+     */
+    public function stream_set_option($option, $arg1, $arg2)
+    {
+    }
+
+    /**
+     *
+     * @return  array
+     */
+    public function stream_stat()
+    {
+    }
+
+    /**
+     *
+     * @return  integer
+     */
+    public function stream_tell()
+    {
+    }
+
+    /**
+     *
+     * @param   string  $data
+     * @return  integer
+     */
+    public function stream_write($data)
+    {
+    }
+
+    /**
+     *
+     * @param   string   $path
+     * @return  boolean
+     */
+    public function unlink($path)
+    {
+    }
+
+    /**
+     *
+     * @param   string  $path
+     * @param   integer $flags
+     * @return  array
+     */
+    public function url_stat($path, $flags)
+    {
     }
 }
 
