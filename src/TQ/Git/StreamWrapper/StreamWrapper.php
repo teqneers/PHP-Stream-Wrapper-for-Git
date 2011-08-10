eb3aa06c37008251cdc6bfa3a625d4e9eb246466
Stefan Gehrig <gehrig@teqneers.de>
2011-08-10 21:57:19 +0200

    added documentation of stat mode bit field (from `man 2 stat`)

--- src/TQ/Git/StreamWrapper/StreamWrapper.php
+++ src/TQ/Git/StreamWrapper/StreamWrapper.php
@@ -575,6 +575,30 @@ class StreamWrapper
     /**
      * streamWrapper::url_stat — Retrieve information about a file
      *
+     * S_IFMT     0170000   bit mask for the file type bit fields
+     * S_IFSOCK   0140000   socket
+     * S_IFLNK    0120000   symbolic link
+     * S_IFREG    0100000   regular file
+     * S_IFBLK    0060000   block device
+     * S_IFDIR    0040000   directory
+     * S_IFCHR    0020000   character device
+     * S_IFIFO    0010000   FIFO
+     * S_ISUID    0004000   set UID bit
+     * S_ISGID    0002000   set-group-ID bit (see below)
+     * S_ISVTX    0001000   sticky bit (see below)
+     * S_IRWXU    00700     mask for file owner permissions
+     * S_IRUSR    00400     owner has read permission
+     * S_IWUSR    00200     owner has write permission
+     * S_IXUSR    00100     owner has execute permission
+     * S_IRWXG    00070     mask for group permissions
+     * S_IRGRP    00040     group has read permission
+     * S_IWGRP    00020     group has write permission
+     * S_IXGRP    00010     group has execute permission
+     * S_IRWXO    00007     mask for permissions for others (not in group)
+     * S_IROTH    00004     others have read permission
+     * S_IWOTH    00002     others have write permission
+     * S_IXOTH    00001     others have execute permission
+     *
      * @param   string  $path   The file path or URL to stat. Note that in the case of a URL, it must be a :// delimited URL.
      *                          Other URL forms are not supported.
      * @param   integer $flags  Holds additional flags set by the streams API. It can hold one or more of the following
