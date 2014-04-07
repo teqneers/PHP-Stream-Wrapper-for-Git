e333026db1fbb62479860c553608b813c0837436
Don Mayo <don.mayo@oracle.com>
2014-04-07 11:59:49 -0400

    Only add file on stream_close() when repo is dirty
    Fixes #17

--- src/TQ/Vcs/StreamWrapper/AbstractStreamWrapper.php
+++ src/TQ/Vcs/StreamWrapper/AbstractStreamWrapper.php
@@ -443,8 +443,8 @@ abstract class AbstractStreamWrapper
         $this->fileBuffer   = null;
 
         $repo   = $this->path->getRepository();
-        $repo->add(array($this->path->getFullPath()));
         if ($repo->isDirty()) {
+            $repo->add(array($this->path->getFullPath()));
             $commitMsg      = $this->getContextOption('commitMsg', null);
             $author         = $this->getContextOption('author', null);
             $repo->commit($commitMsg, array($this->path->getFullPath()), $author);
